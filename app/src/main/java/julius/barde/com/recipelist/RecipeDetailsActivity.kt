584fd3915378b715a279a6e33e72b8e5db8ea51a
Your Name <yam1434414344@gmail.com>
2017-12-08 21:52:54 -0800

    final commit

---
+++ app/src/main/java/julius/barde/com/recipelist/RecipeDetailsActivity.kt
@@ -0,0 +1,23 @@
+package julius.barde.com.recipelist
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.recipe_activity.*
+
+class RecipeActivityActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.recipe_activity)
+
+        val recipe:Recipe = intent.getParcelableExtra("123")
+        photoTV.setImageResource(recipe.photo)
+        ingredientsTV.text= recipe.ingredients
+        proceduresTV.text= recipe.procedures
+
+
+    }
+
+}
